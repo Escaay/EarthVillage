596c25ad1a34846bd1df7708e492ef09c899366b
escaay <qwj2790751195@gmail.com>
2024-09-25 01:16:14 +0800

    feat: 二次封装axios-30%

---
+++ types/axios.ts
@@ -0,0 +1,6 @@
+export type Res = {
+  code: number;
+  status?: string;
+  message: string;
+  data?: any;
+};
---
+++ utils/axios.ts
@@ -0,0 +1,3 @@
+import axiosConfig from '../config/axios';
+import axios from 'axios';
+export default axios.create(axiosConfig);
