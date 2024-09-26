759805fabcb1240688b92c42770d16596e678d78
escaay <qwj2790751195@gmail.com>
2024-09-26 11:37:57 +0800

    fix: 增加请求头数据格式

--- config/axios.ts
+++ config/axios.ts
@@ -1,5 +1,5 @@
 export default {
   baseURL: 'https://1258291989-2vrzc9e2n3.ap-guangzhou.tencentscf.com', // 线上联调
   timeout: 1000,
-  headers: { 'X-Custom-Header': 'foobar' },
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 };
