9a40430c61b493bfc9baa42585b5fb044cf3e75a
escaay <qwj2790751195@gmail.com>
2024-10-14 20:54:52 +0800

    feat: 改变判断登录的方式，增加一堆接口

--- component/RouterGuard.tsx
+++ component/RouterGuard.tsx
@@ -1,11 +1,9 @@
-import { useEffect } from 'react';
-import { useMyInfo } from '../store/my-info';
-import { useIsLogin } from '../store/islogin';
+import { useMyInfo } from '../store/myInfo';
 import { useNavigation } from '@react-navigation/native';
 import Login from '../pages/me/login';
 // 不直接使用storage判断的原因是如果token失效，需要立刻在前端体现出变化，需要把他变成一个状态
 export default (props: any) => {
-  const navigation = useNavigation<any>();
-  const isLogin = useIsLogin();
+  const isLogin = !!useMyInfo().id;
+
   return <>{isLogin ? props.children : <Login></Login>}</>;
 };
--- config/axios.ts
+++ config/axios.ts
@@ -2,7 +2,7 @@ export default {
   // baseURL: 'https://ev-server-whfmnthsix.cn-hangzhou.fcapp.run/api', // 线上联调
   // baseURL: 'http://192.168.12.74:3000/api', // 热点本地联调
   baseURL: __DEV__
-    ? 'http://192.168.1.100:3000/api'
+    ? 'http://192.168.1.104:3000/api'
     : 'https://ev-server-whfmnthsix.cn-hangzhou.fcapp.run/api',
   timeout: 5000,
   retry: 3,
