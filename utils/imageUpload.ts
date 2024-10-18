d069ea2c8cce9b52ccc158821980dd75c5050f93
escaay <qwj2790751195@gmail.com>
2024-10-19 04:48:12 +0800

    feat: 上传和裁剪图片封装

--- utils/imageUpload.ts
+++ utils/imageUpload.ts
@@ -1,16 +1,23 @@
 import ImagePicker from 'react-native-image-crop-picker';
-export const uploadSingleImg = async () => {
+export const uploadSingleImg = async (
+  isMultiple: boolean = true,
+  width?: any,
+  height?: any,
+) => {
   try {
     const img = await ImagePicker.openPicker({
-      width: 300,
-      height: 300,
+      width,
+      height,
       compressImageQuality: 1,
       cropping: true,
+      // compressImageMaxHeight: 500,
       includeBase64: true,
       mediaType: 'photo',
+      multiple: isMultiple,
     });
     return img;
   } catch (e) {
+    console.log(e);
     return Promise.reject(e);
   }
 };
