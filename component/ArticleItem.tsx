5e483ff39811f82b7963150814289f17439960bd
escaay <qwj2790751195@gmail.com>
2024-11-28 15:42:13 +0800

    feat: 增加组队加入队伍功能和展示

--- component/ArticleItem.tsx
+++ component/ArticleItem.tsx
@@ -14,9 +14,14 @@ import DefaultText from './DefaultText';
 import { useNavigation } from '@react-navigation/native';
 import { queryUserBasis } from '../api/user';
 import { useRoute } from '@react-navigation/native';
-import { updateArticle } from '../api/article';
+import {
+  createArticleLike,
+  deleteArticleLike,
+  updateArticle,
+} from '../api/article';
 import publishDisplayTime from '../utils/publishDisplayTime';
 import dayjs from 'dayjs';
+import { v4 as uuidV4 } from 'uuid';
 import {
   useRecommandArticleList,
   setRecommandArticleList,
@@ -30,12 +35,16 @@ import {
   setSameCityArticleList,
 } from '../store/sameCityArticleList';
 import basic from '../config/basic';
+import { useWebsocket } from '../store/websocket';
+import BasicButton from './BasicButton';
+import { opacity } from 'react-native-reanimated/lib/typescript/Colors';
 
 const CONTENT_PADDING_LEFT = 40 + 6; // 头像宽度+右margin
 
 const ArticleItem = (props: any) => {
   const myInfo = useMyInfo();
   const route = useRoute();
+  const websocket = useWebsocket();
   const recommandArticleList = useRecommandArticleList();
   const sameCityArticleList = useSameCityArticleList();
   const navigation = useNavigation<any>();
@@ -67,15 +76,38 @@ const ArticleItem = (props: any) => {
   const clickLike = async (
     articleId: string,
     likeInfo: any,
-    isLiked: boolean,
+    hasLike: boolean,
   ) => {
     try {
-      const newLikeInfo = (articleItemData.likeInfo = isLiked
-        ? likeInfo.filter(item => item.userId !== myInfo.id)
-        : [
-            ...likeInfo,
-            { userId: myInfo.id, isRead: false, createTime: new Date() },
-          ]);
+      let newLikeInfo;
+      const newArticleLikeItem = {
+        articleLikeId: uuidV4(),
+        articleId,
+        articleSenderId: articleItemData.senderId,
+        articleTextContent: articleItemData.textContent,
+        senderId: myInfo.id,
+        senderGender: myInfo.gender,
+        isRead: false,
+        senderAge: myInfo.age,
+        senderCurrentAddress: myInfo.currentAddress,
+        senderAvatarURL: myInfo.avatarURL,
+        senderName: myInfo.name,
+        createTime: new Date(),
+        updateTime: new Date(),
+      };
+      if (hasLike) {
+        newLikeInfo = likeInfo.filter(item => item.senderId !== myInfo.id);
+      } else {
+        newLikeInfo = [
+          ...likeInfo,
+          {
+            articleId,
+            senderId: newArticleLikeItem.senderId,
+            articleLikeId: newArticleLikeItem.articleLikeId,
+          },
+        ];
+      }
+      articleItemData.likeInfo = newLikeInfo;
       // 匹配两个动态列表和用户文章列表中的笔记，如果存在则更新点赞信息
       const matchRecommandArticleItem = recommandArticleList?.find(
         item => item.articleId === articleItemData.articleId,
@@ -109,10 +141,26 @@ const ArticleItem = (props: any) => {
         matchOthersArticleList.likeInfo = newLikeInfo;
         setUserArticleList({ ...userArticleList });
       }
-      await updateArticle({
-        articleId,
-        likeInfo: newLikeInfo,
-      });
+
+      // 更新数据库点赞信息
+      if (hasLike) {
+        // 删除点赞记录
+        await deleteArticleLike({
+          articleLikeId: likeInfo.find(item => item.senderId === myInfo.id)
+            .articleLikeId,
+        });
+      } else {
+        await createArticleLike(newArticleLikeItem);
+        // 发给websocket消息
+        websocket.send(
+          JSON.stringify({
+            type: 'likeArticle',
+            data: {
+              receiverId: newArticleLikeItem.articleSenderId,
+            },
+          }),
+        );
+      }
     } catch (e) {
       console.log(e);
     }
@@ -129,21 +177,47 @@ const ArticleItem = (props: any) => {
     senderGender,
     senderCurrentAddress,
     senderAge,
+    likeInfo,
     textContent,
     maxPaticipant,
-    likeInfo,
     currentPaticipant,
     imageUrlList,
     createTime,
     updateTime,
     commentNum,
+    viewNum,
+    tag,
+    teamPeople,
+    peopleNum,
+    playTime,
+    gameName,
   } = articleItemData;
 
-  const isLiked = likeInfo.some(item => item.userId === myInfo.id);
-  // console.log('isLiked', isLiked, likeInfo)
+  const diaplayPlayTime = () => {
+    const dayjsTime = dayjs(playTime);
+    switch (Number(dayjsTime.day()) - Number(dayjs(new Date()).day())) {
+      case 0:
+        return '今天' + dayjsTime.format('H:mm');
+      case 1:
+        return '明天' + dayjsTime.format('H:mm');
+      case 2:
+        return '后天' + dayjsTime.format('H:mm');
+      default:
+        return dayjsTime.format('MM/DD H:mm');
+    }
+  };
+
+  const clickPeopleAvatar = peopleId => {};
+
+  const clickJoinTeam = articleId => {};
+
+  const hasLike = likeInfo.some(item => item.senderId === myInfo.id);
   return (
     <>
-      <Modal visible={isPreviewImage} transparent={true}>
+      <Modal
+        visible={isPreviewImage}
+        transparent={true}
+        onRequestClose={() => setIsPreviewImage(false)}>
         <ImageViewer
           onClick={() => setIsPreviewImage(false)}
           index={previewImageIndex}
@@ -231,7 +305,68 @@ const ArticleItem = (props: any) => {
             </View>
           </View>
         </View>
-        <View style={{ paddingLeft: CONTENT_PADDING_LEFT }}>
+        <View
+          style={{ paddingLeft: CONTENT_PADDING_LEFT - 10, paddingRight: 5 }}>
+          <View style={{ flexDirection: 'row' }}>
+            <DefaultText style={{ color: 'gray', fontSize: 14, flex: 4 }}>
+              # {tag}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {peopleNum ? '# ' + peopleNum + '人' : ''}
+            </DefaultText>
+            {playTime ? (
+              <BasicButton
+                style={{
+                  color: 'gray',
+                  flex: 2,
+                  fontSize: 14,
+                  backgroundColor: basic.themeColor,
+                  opacity: 0.8,
+                }}
+                height={20}>
+                {diaplayPlayTime()}
+              </BasicButton>
+            ) : null}
+          </View>
+          <WhiteSpace></WhiteSpace>
+          {teamPeople ? (
+            <View style={{ flexDirection: 'row' }}>
+              {teamPeople.map(item => (
+                <Pressable
+                  key={item ? item.peopleId : uuidV4()}
+                  onPress={() =>
+                    item ? clickPeopleAvatar(item.peopleId) : () => {}
+                  }>
+                  <Image
+                    style={{
+                      marginRight: 4,
+                      width: 30,
+                      height: 30,
+                      borderRadius: 15,
+                    }}
+                    source={
+                      item
+                        ? {
+                            uri: item.peopleAvatarURL,
+                          }
+                        : require('../assets/img/people.png')
+                    }
+                  />
+                </Pressable>
+              ))}
+              <Pressable onPress={() => clickJoinTeam(articleId)}>
+                <Image
+                  style={{
+                    marginRight: 4,
+                    width: 30,
+                    height: 30,
+                    borderRadius: 15,
+                  }}
+                  source={require('../assets/img/joinTeam.png')}
+                />
+              </Pressable>
+            </View>
+          ) : null}
+          <WhiteSpace></WhiteSpace>
+          <WhiteSpace></WhiteSpace>
           <DefaultText style={{ color: 'black', fontSize: 14 }}>
             {textContent}
           </DefaultText>
@@ -243,8 +378,8 @@ const ArticleItem = (props: any) => {
                   style={{
                     marginRight: 12,
                     marginBottom: 10,
-                    width: 120,
-                    height: 120,
+                    width: 100,
+                    height: 100,
                     borderRadius: 10,
                   }}
                   source={{
@@ -263,8 +398,15 @@ const ArticleItem = (props: any) => {
             <DefaultText style={{ flex: 4, fontSize: 12 }}>
               {publishDisplayTime(createTime)}
             </DefaultText>
-            <View style={{ flex: 2, flexDirection: 'row' }}>
-              <View style={{ flexDirection: 'row', marginRight: 20 }}>
+            <View style={{ flex: 3, flexDirection: 'row' }}>
+              <View style={{ flexDirection: 'row', marginRight: 10 }}>
+                <Icon name="eye" color={undefined} style={{ fontSize: 21 }} />
+                <DefaultText isCenter={true} style={{ fontSize: 12 }}>
+                  {' '}
+                  {viewNum}
+                </DefaultText>
+              </View>
+              <View style={{ flexDirection: 'row', marginRight: 10 }}>
                 <Icon
                   name="comment"
                   color={undefined}
@@ -277,11 +419,11 @@ const ArticleItem = (props: any) => {
               </View>
               <Pressable
                 style={{ flexDirection: 'row', marginRight: 10 }}
-                onPress={() => clickLike(articleId, likeInfo, isLiked)}>
+                onPress={() => clickLike(articleId, likeInfo, hasLike)}>
                 <Icon
                   name="like"
                   style={{ fontSize: 20 }}
-                  color={isLiked ? 'red' : undefined}
+                  color={hasLike ? 'red' : undefined}
                 />
                 <DefaultText isCenter={true} style={{ fontSize: 12 }}>
                   {' '}
@@ -292,6 +434,8 @@ const ArticleItem = (props: any) => {
           </View>
         </View>
       </Pressable>
+      <WhiteSpace></WhiteSpace>
+      <WhiteSpace></WhiteSpace>
       <WhiteSpace></WhiteSpace>
     </>
   );
