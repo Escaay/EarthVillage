3a66a5f27df8a7e1b07ce78f98d759144f374a1d
escaay <qwj2790751195@gmail.com>
2024-12-03 14:10:27 +0800

    feat: 展示更多回复改版

--- component/CommentItem.tsx
+++ component/CommentItem.tsx
@@ -19,6 +19,7 @@ import basic from '../config/basic';
 import {
   createArticleCommentLike,
   deleteArticleCommentLike,
+  queryReplyCommentList,
   updateArticleComment,
 } from '../api/article';
 import publishDisplayTime from '../utils/publishDisplayTime';
@@ -34,25 +35,39 @@ const CommentItem = (props: any) => {
   const [isPreviewImage, setIsPreviewImage] = useState(false);
   const navigation = useNavigation<any>();
   // hasOrigin标志是否需要在评论下方显示原文
-  const { commentItemData, updateCommentList, hasOrigin } = props;
+  const { commentItemData, updateCommentList, hasOrigin, clickComment } = props;
   const {
     articleCommentId,
     sender,
     textContent,
     article,
-    articleLike,
     createTime,
+    imageURL,
+    level,
+    replyCommentCount,
+    replyComment,
+    articleCommentLike,
   } = commentItemData;
-  const imageURL = article.imageUrlList[0];
-  const isLiked = articleLike.some(item => item.sender.id === myInfo.id);
+  const isLiked = articleCommentLike.some(item => item.sender.id === myInfo.id);
+  const loadReplyComment = async () => {
+    const moreReplyCommentData = (
+      await queryReplyCommentList({
+        replyArticlCommentId: articleCommentId,
+        skip: replyComment.length,
+      })
+    ).data;
+    commentItemData.replyComment = [...replyComment, ...moreReplyCommentData];
+    updateCommentList();
+  };
+
   const clickLikeComment = async () => {
     try {
       if (isLiked) {
         // 取消点赞
-        const matchItem = articleLike.find(
+        const matchItem = articleCommentLike.find(
           item => item.sender.id === myInfo.id,
         );
-        commentItemData.articleLike = articleLike.filter(
+        commentItemData.articleCommentLike = articleCommentLike.filter(
           item => item !== matchItem,
         );
         updateCommentList();
@@ -85,8 +100,8 @@ const CommentItem = (props: any) => {
           // isRead: false,
           // createTime: new Date(),
         };
-        commentItemData.articleLike = [
-          ...articleLike,
+        commentItemData.articleCommentLike = [
+          ...articleCommentLike,
           {
             articleCommentLikeId: newCommentLike.articleCommentLikeId,
             sender: {
@@ -98,6 +113,7 @@ const CommentItem = (props: any) => {
         const createArticleCommentLikePayload: any = {
           articleCommentLikeId: newCommentLike.articleCommentLikeId,
           commentSenderId: commentItemData.sender.id,
+          isRead: false,
           sender: {
             connect: {
               id: newCommentLike.sender.id,
@@ -105,7 +121,7 @@ const CommentItem = (props: any) => {
           },
           article: {
             connect: {
-              articleId: article.id,
+              articleId: article.articleId,
             },
           },
           articleComment: {
@@ -203,7 +219,18 @@ const CommentItem = (props: any) => {
           </View>
         </View>
       </Pressable>
-      <View style={{ paddingLeft: CONTENT_PADDING_LEFT }}>
+
+      <Pressable
+        onPress={() => clickComment(sender.name, level, articleCommentId)}
+        style={{ paddingLeft: CONTENT_PADDING_LEFT }}>
+        {route.name === 'ArticleDetail' ? null : (
+          <>
+            <DefaultText style={{ color: 'gray', fontSize: 12 }}>
+              {level === 1 ? '评论了你的动态' : '回复了你的评论'}
+            </DefaultText>
+            <WhiteSpace></WhiteSpace>
+          </>
+        )}
         <DefaultText style={{ color: 'black', fontSize: 14 }}>
           {textContent}
         </DefaultText>
@@ -211,9 +238,19 @@ const CommentItem = (props: any) => {
         {route.name === 'ArticleDetail' ? null : (
           <>
             <WhiteSpace></WhiteSpace>
-            <DefaultText style={{ color: 'gray', fontSize: 12 }}>
-              {article.textContent}
-            </DefaultText>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <View
+                style={{
+                  backgroundColor: 'rgba(45, 45, 45, 0.1)',
+                  width: 8,
+                  height: 20,
+                  marginRight: 6,
+                  borderRadius: 5,
+                }}></View>
+              <DefaultText style={{ color: 'gray', fontSize: 12 }}>
+                {article.textContent}
+              </DefaultText>
+            </View>
           </>
         )}
         <WhiteSpace></WhiteSpace>
@@ -252,7 +289,6 @@ const CommentItem = (props: any) => {
           </View>
         ) : null}
         <WhiteSpace></WhiteSpace>
-        <WhiteSpace></WhiteSpace>
         <View style={{ flexDirection: 'row' }}>
           <DefaultText style={{ flex: 4, fontSize: 12 }}>
             {publishDisplayTime(createTime)}
@@ -272,12 +308,186 @@ const CommentItem = (props: any) => {
             />
             <DefaultText style={{ fontSize: 12 }} isCenter={true}>
               {' '}
-              {articleLike.length}
+              {articleCommentLike.length}
             </DefaultText>
           </Pressable>
         </View>
-      </View>
+      </Pressable>
       <WhiteSpace></WhiteSpace>
+      <WhiteSpace></WhiteSpace>
+
+      {route.name === 'ArticleDetail' && replyComment?.length ? (
+        <View>
+          {replyComment.map(item => {
+            const {
+              articleCommentId,
+              sender,
+              textContent,
+              article,
+              createTime,
+              imageURL,
+              level,
+              // replyComment,
+              articleCommentLike,
+            } = item;
+            return (
+              <View key={articleCommentId}>
+                <Pressable
+                  onPress={() => clickAvatar(sender.id)}
+                  style={{
+                    flexDirection: 'row',
+                    paddingLeft: CONTENT_PADDING_LEFT - 10,
+                    marginBottom: 15,
+                  }}>
+                  <Image
+                    style={{
+                      marginRight: 6,
+                      width: 30,
+                      height: 30,
+                      borderRadius: 15,
+                    }}
+                    source={
+                      sender.avatarURL
+                        ? {
+                            uri: sender.avatarURL,
+                          }
+                        : require('../assets/img/avatar.png')
+                    }
+                  />
+                  <View style={{ justifyContent: 'space-around' }}>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                      }}>
+                      <DefaultText
+                        style={{
+                          fontSize: 10,
+                          marginRight: 10,
+                          color: 'black',
+                          fontWeight: 800,
+                        }}
+                        isCenter={true}>
+                        {sender.name}
+                      </DefaultText>
+                      <DefaultText
+                        style={{ fontSize: 10, marginRight: 2, color: 'black' }}
+                        isCenter={true}>
+                        {sender.age}
+                      </DefaultText>
+                      <Icon
+                        name={sender.gender === '男' ? 'man' : 'woman'}
+                        style={{
+                          textAlignVertical: 'center',
+                          fontSize: 12,
+                          color:
+                            sender.gender === '男'
+                              ? basic.manIconColor
+                              : basic.womanIconColor,
+                        }}></Icon>
+                    </View>
+                    <View
+                      style={{ flexDirection: 'row', alignItems: 'center' }}>
+                      <Icon
+                        name="environment"
+                        size="xxs"
+                        style={{
+                          position: 'relative',
+                          top: 1,
+                          fontSize: 12,
+                        }}></Icon>
+                      <DefaultText style={{ fontSize: 8 }} isCenter={true}>
+                        {sender.currentAddress
+                          ?.filter(item => item !== '全部')
+                          .join('-')}
+                      </DefaultText>
+                    </View>
+                  </View>
+                </Pressable>
+                <Pressable
+                  onPress={() =>
+                    clickComment(sender.name, level, articleCommentId)
+                  }
+                  style={{ paddingLeft: CONTENT_PADDING_LEFT }}>
+                  <DefaultText style={{ color: 'black', fontSize: 12 }}>
+                    {textContent}
+                  </DefaultText>
+
+                  {imageURL ? (
+                    <View style={{ backgroundColor: 'white' }}>
+                      <Modal
+                        visible={isPreviewImage}
+                        transparent={true}
+                        onRequestClose={() => setIsPreviewImage(false)}>
+                        <ImageViewer
+                          onClick={() => setIsPreviewImage(false)}
+                          index={0}
+                          saveToLocalByLongPress
+                          onSaveToCamera={() => {
+                            Toast.info('保存成功');
+                          }}
+                          menuContext={{
+                            saveToLocal: '保存至手机',
+                            cancel: '取消',
+                          }}
+                          imageUrls={[{ url: imageURL }]}
+                        />
+                      </Modal>
+                      <Pressable onPress={() => setIsPreviewImage(true)}>
+                        <Image
+                          style={{
+                            width: 50,
+                            height: 50,
+                            borderRadius: 10,
+                          }}
+                          source={{
+                            uri: imageURL,
+                          }}
+                        />
+                      </Pressable>
+                    </View>
+                  ) : null}
+                  <WhiteSpace></WhiteSpace>
+                  <View style={{ flexDirection: 'row' }}>
+                    <DefaultText style={{ flex: 4, fontSize: 10 }}>
+                      {publishDisplayTime(createTime)}
+                    </DefaultText>
+                    <Pressable
+                      style={{
+                        flexDirection: 'row',
+                        justifyContent: 'flex-end',
+                        marginRight: 10,
+                        flex: 2,
+                      }}
+                      onPress={clickLikeComment}>
+                      <Icon
+                        name="like"
+                        style={{ fontSize: 16 }}
+                        color={isLiked ? 'red' : undefined}
+                      />
+                      <DefaultText style={{ fontSize: 12 }} isCenter={true}>
+                        {' '}
+                        {articleCommentLike.length}
+                      </DefaultText>
+                    </Pressable>
+                  </View>
+                </Pressable>
+                <WhiteSpace></WhiteSpace>
+              </View>
+            );
+          })}
+
+          {replyComment.length < replyCommentCount ? (
+            <Pressable onPress={() => loadReplyComment()}>
+              <DefaultText
+                style={{ fontSize: 13, paddingLeft: CONTENT_PADDING_LEFT }}>
+                展开更多回复
+              </DefaultText>
+            </Pressable>
+          ) : null}
+
+          <WhiteSpace></WhiteSpace>
+        </View>
+      ) : null}
     </>
   );
 };
